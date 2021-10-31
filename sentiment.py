888ab5ba2856070f125963c2e5443df148dcf811
Cynthia2019 <44718081+Cynthia2019@users.noreply.github.com>
2021-10-31 15:52:19 -0500

    Add files via upload

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 from textblob import TextBlob
+import pickle
+
+drop_list = ['by', 'in', 'a', '-', 'or', 'an', ',', 'for', 'role','made']
+with open("official_list.txt", 'rb') as f:
+    OFFICIAL_AWARDS_1315, OFFICIAL_AWARDS_1819 = pickle.load(f)
+
+def get_data(year):
+    name = 'gg{}.json'.format(year)
+    return pd.read_json(name)
 
 def get_list_from_dict(d):
     l = []
@@ -8,6 +17,17 @@ def get_list_from_dict(d):
         if type(d[item]) != list and d[item] not in l: 
             l.append(d[item])
     return l
+
+def convert_official_to_dict(official): 
+    official_to_abbr = {}
+    for index, row in enumerate(official): 
+        abbr = row.split(" ")
+        abbr = [word for word in abbr if word not in drop_list]
+        for word in row: 
+            if word == 'television':
+                row.append('tv')
+        official_to_abbr[row] = abbr
+    return official_to_abbr
 
 def get_tweets_from_list(df, lis): 
     df = df.drop(['user','id','timestamp_ms'], axis=1)
@@ -38,10 +58,18 @@ def get_sentiment(df, lis):
             result_sentiment[winner]= 'neutral'
     return result_sentiment
 
-def get_sentiments(df, winner_dict): 
+def get_sentiments(year, winner_dict):
+    df = get_data(year)
+    if year <= 2015:
+        awards_dict = convert_official_to_dict(OFFICIAL_AWARDS_1315)
+    else:
+        awards_dict = convert_official_to_dict(OFFICIAL_AWARDS_1819)
     winner_list = get_list_from_dict(winner_dict)
-    winner_df = get_tweets_from_list(df, winner_list)
-    winner_df = winner_df.dropna(how='all')
+    winner_df = get_tweets_from_list(df, winner_list).dropna(how='all')
     winner_sentiment = get_sentiment(winner_df, winner_list)
+
+    # award_list = get_list_from_dict(awards_dict)
+    # award_df = get_tweets_from_list(df, award_list).dropna(how='all')
+    # award_sentiment = get_sentiment(award_df, award_list)
     return winner_sentiment
             
