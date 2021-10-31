71e01c6c98fe58234c04da13bb794e1899c7c1c8
dmarquette2022 <davidmarquette2022@u.northwestern.edu>
2021-10-31 18:21:28 -0500

    Finalized Nominee Comber

--- NomineeComber.py
+++ NomineeComber.py
@@ -7,19 +7,41 @@ import json
 import nltk
 import string
 import spacy
-
+import re
+import pickle
 from nltk import word_tokenize, pos_tag
-def NomGather(awardName, awardKeys):
+def quickNomGather(personOrMovie, year):
+    print("started quick search")
+    file_name = 'gg{}.json'.format(year)
+    pand = pandas.read_json(file_name)
+    criteria = ["nominated for", "nominee for", "congrats to", "should have won", "wins", "nominee"]
+    tempTotal = []
+    for item in criteria:
+        brandy = pand[pand['text'].str.contains(item)]
+        tempList = brandy['text'].tolist()
+        tempTotal += tempList
+    
+    finCand = tempTotal
+    print("starting findNN")
+    nomCand = quickFindNN(finCand, "person")
+    print("finished findNN")
+    print("Finished Quick Search")
+    return nomCand
+    
+    print("finished quick search")
+    
+def NomGather(personOrMovie, scoreDict, year):
+    print("Started loading tweets")
     basic_words = ["was", "an", "or", "a", "the", "more", 'to', "for", "golden", "globe", 's"', "in", "that", "you", "ever", "as", "be", "i", "at", "ben", "with", "and", "but", "&amp;", "is", "of", "those", "he", "she", "rt", "have", "all", "we", "so", "it", "on", "by", "les", "dress", "dressed", "this", "her"]
     punctuation = ["'", "*", ",", '"', ".", ":"]
+    file_name = 'gg{}.json'.format(year)
+    pand = pandas.read_json(file_name)
+
     
-    df2013 = pandas.read_json("c:/Users/dmarq/Downloads/GG Scanner/gg2013.json")
     nomCand = []
     finCand = []
-    awardName = awardName.lower()
-    for i in range(len(awardKeys)):
-        awardKeys[i] = awardKeys[i].lower()
-    for row in df2013.itertuples():
+    
+    for row in pand.itertuples():
         
         dontCare = False
         tweet = row[1]
@@ -30,41 +52,118 @@ def NomGather(awardName, awardKeys):
         cnt = 0
         if "rt" in tweet:
             continue
-        if "nomination" in tweet or "nominated" in tweet or "nominee"  in tweet or "congrats" in tweet or "wins" in tweet or "should" in tweet:
+        #if "nomination" in tweet or "nominated" in tweet or "nominee"  in tweet or "congrats" in tweet or "wins" in tweet:
+        #    nomCand.append(tweet)
+        if "nominated for" in tweet or "nominee for" in tweet or "congrats to" in tweet or "should have won" in tweet:
             nomCand.append(tweet)
+        elif "wins" in tweet or "nominee" in tweet:
+            nomCand.append(tweet)
+        
+        
+    #if len(awardName) < 4:
+    #    for can in nomCand:
+    #        count = 0
+    #        count2 = 0
+    #        if awardName in can:
+    #            finCand.append(can)
+    #else:
+    #    for can in nomCand:
+    #        count = 0
+    #        count2 = 0
+    #        if awardName in can:
+    #            for item in can:
+    #                if item in awardKeys:
+    #                    count += scoreDict[item]
+    #                    count2 += 1
+    #        if count >= 12 and count2 > 4:
+    #            finCand.append(" ".join(can))
     
-    for can in nomCand:
-        count = 0
-        can = can.split()
-        for item in can:
-            if item in awardKeys:
-                count+=1
-        if count >= len(awardKeys)-2:
-            finCand.append(" ".join(can))
     
 
         
-        
-    nomCand = findNN(finCand)
+    finCand = nomCand    
+    #nomCand = findNN(finCand, personOrMovie)
+    nomCand = quickFindNN(finCand, "person")
+    #with open('prereadtweets.txt', 'wb') as f:
+    #    pickle.dump(nomCand, f)
+    print("Finished Loading Tweets")
     return nomCand
 
-def findNN(nomCand):
+def findNN(nomCand, person):
     nnCand = []
     nlp = spacy.load("en_core_web_sm")
-    
+    if person == "person":
+        posType = "PROPN"
+    else:
+        posType = "NOUN"
+
     for can in nomCand:
         tweet = nlp(can)
         for i, tagged_token in enumerate(tweet):
             tempText = ""
-            if tagged_token.pos_ == 'PROPN':
+            if tagged_token.pos_ == posType:
                 tempText = tagged_token.text
-                k=i
-                while k<(len(tweet)-1) and tweet[k+1].pos_ == 'PROPN':
-                        tempText = tagged_token.text + " " + tweet[k+1].text
+                k=i+1
+                while k<(len(tweet)-1) and "#" not in tweet.text and "http" not in tweet.text:
+                        tempText = tempText + " " + tweet[k].text
                         k=k+1
+                break
                 #print("TWEET: " + " ".join(tweet) + " NAME: " +tagged_token[0])
-                nnCand.append(tempText)
+        nnCand.append(tempText)
     return nnCand
+
+def quickFindNN(nomCand, person):
+    nnCand = []
+    nlp = spacy.load("en_core_web_sm")
+    if person == "person":
+        posType = "PROPN"
+    else:
+        posType = "NOUN"
+    
+    for doc in nlp.pipe(nomCand):
+    # Do something with the doc here
+        for i, ent in enumerate(doc):
+            if ent.pos_ == posType:
+                tempText = ent.text
+                k=i+1
+                while k<(len(doc)-1) and "#" not in doc[k].text and "http" not in doc[k].text:
+                        tempText = tempText + " " + doc[k].text
+                        k=k+1
+                break
+        nnCand.append(tempText)
+    
+    return nnCand
+    
+
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f"  # dingbats
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+def add_flatten_lists(the_lists):
+    result = []
+    for _list in the_lists:
+        result += _list
+    return result
         
 
 
---
+++ NomineeGather.py
@@ -0,0 +1,412 @@
+from json import decoder
+import pickle
+from nltk.grammar import is_nonterminal
+import pandas
+import itertools
+from CanGather import canGather
+from VoteSystem import voteCounter, sortVote
+from trimmer import FiveWord, endRemove, fatTrimmer, nipNames, purgeSame, removePunct, unifySimilar, unifySpaces, purgeSimilar, trimToBest, purgeSame
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+from wordVote import commonality
+from NomineeComber import NomGather, quickNomGather
+from operator import itemgetter, pos
+from collections import Counter
+import spacy
+import json
+nominees = dict()
+mAwards = ['Best Motion Picture - Drama','Best Motion Picture - Musical or Comedy','Best Director','Best Actor - Motion Picture Drama','Best Actor - Motion Picture Musical or Comedy',
+'Best Actress - Motion Picture Drama','Best Actress - Motion Picture Musical or Comedy','Best Supporting Actor - Motion Picture','Best Supporting Actress - Motion Picture',
+'Best Screenplay','Best Original Score','Best Foreign Language Film','Best Animated Feature Film','Cecil B. DeMille Lifetime Achievement Award', 'Best Original Song']
+
+# Television Awards
+tAwards = ['Best Drama Series','Best Comedy Series','Best Actor in a Television Drama Series','Best Actor in a Television Comedy Series','Best Actress in a Television Drama Series',
+'Best Actress in a Television Comedy Series','Best Limited Series or Motion Picture made for Television','Best Actor in a Limited Series made for Television',
+'Best Actress in a Limited Series made for Television','Best Supporting Actor in a Limited Series made for Television',
+'Best Supporting Actress in a Limited Series made for Television']
+
+catToAwards = {"Cecil B. DeMille Award" : "Cecil B. DeMille Award",
+"Best Motion Picture Drama" : "Best Motion Picture - Drama",
+"Best Actress Motion Picture Drama": "Best Performance by an Actress in a Motion Picture - Drama",
+"Best Actor Motion Picture Drama" : "Best Performance by an Actor in a Motion Picture - Drama",
+"Best Motion Picture Musical or Comedy": "Best Motion Picture - Comedy Or Musical",
+"Best Actress Motion Picture Musical or Comedy" :"Best Performance by an Actress in a Motion Picture - Comedy Or Musical",
+"Best Actor Motion Picture Musical or Comedy" :"Best Performance by an Actor in a Motion Picture - Comedy Or Musical",
+"Best Animated Feature Film": "Best Animated Feature Film",
+"Best Foreign Language Film" : "Best Foreign Language Film",
+"Best Supporting Actress Motion Picture": "Best Performance by an Actress In A Supporting Role in a Motion Picture",
+"Best Supporting Actor Motion Picture" : "Best Performance by an Actor In A Supporting Role in a Motion Picture",
+"Best Director": "Best Director - Motion Picture",
+"Best Screenplay": "Best Screenplay - Motion Picture",
+"Best Original Score": "Best Original Score - Motion Picture",
+"Best Original Song" : "Best Original Song - Motion Picture",
+"Best Drama Series" :"Best Television Series - Drama",
+"Best Actress in a Television Drama Series": "Best Performance by an Actress In A Television Series - Drama",
+"Best Actor in a Television Drama Series" : "Best Performance by an Actor In A Television Series - Drama",
+"Best Comedy Series" : "Best Television Series - Comedy Or Musical",
+"Best Actress in a Television Comedy Series" :"Best Performance by an Actress In A Television Series - Comedy Or Musical",
+"Best Mini-Series or Motion Picture made for Television" : "Best Mini-Series Or Motion Picture Made for Television",
+"Best Actor in a Television Comedy Series" : "Best Performance by an Actor In A Television Series - Comedy Or Musical",
+"Best Actress in a Mini-Series or Motion Picture made for Television" : "Best Performance by an Actress In A Mini-series or Motion Picture Made for Television",
+"Best Actor in a Mini-Series or Motion Picture made for Television" : "Best Performance by an Actor in a Mini-Series or Motion Picture Made for Television",
+"Best Supporting Actress in a Series, Mini-Series or Motion Picture made for Television": "Best Performance by an Actress in a Supporting Role in a Series, Mini-Series or Motion Picture Made for Television",
+"Best Supporting Actor in a Series, Mini-Series or Motion Picture made for Television" : "Best Performance by an Actor in a Supporting Role in a Series, Mini-Series or Motion Picture Made for Television"}
+
+
+
+basicAwards = list(catToAwards.keys())
+awardsAndNomCands = dict()
+def get_tweets(year):
+    
+    # get tweets
+    with open('gg%s.json' % year) as json_data:
+        data = json.load(json_data)
+    json_data.close()
+    
+    #stopWords = set(stopwords.words('english'))
+    stopWords_tweets = ['http','rt','goldenglobes','golden','globes','rt','golden','globes','globe' 
+                        'goldenglobes','goldenglobe']    
+    keywords = ['best','winner','wins','award','awards','nominees','nominated','congratulates',
+                'congratulations','congrats','announce','announcing','present']
+
+    
+    # tokenize tweets and select matching tweets
+    start = 0
+    end = len(data)
+    word_list = []  
+    for q in range(start, end):
+        words = word_tokenize(data[q]['text'])
+        
+        # convert to lower case
+        text = [words[i].lower() for i in range(len(words))]
+        
+        words_meaningful = [token for token in text if token not in stopWords_tweets and token.isalpha()]
+        if len(list(set(words_meaningful) & set(keywords)))>0:
+            word_list.append(words_meaningful)
+    
+    return word_list
+
+
+def translate(finalDict):
+    tempDict = {}
+    for item in finalDict:
+        real = catToAwards[item]
+        real = real.lower()
+        tempDict[real] = finalDict[item]
+    return tempDict
+
+def add_flatten_lists(the_lists):
+    result = []
+    for _list in the_lists:
+        result += _list
+    return result
+
+def get_awardsKeywords(awards):
+    awards_keys = awards
+    awards_keywords = []
+    keyDict = {}
+    for i in range(len(awards_keys)):
+        # tokenize awards words
+        awards_token = word_tokenize(awards_keys[i])
+        # find awards keywords
+        awards_temp = []
+        stopWords_awards = ['by', 'an', 'in', 'a', 'for', 'or', '-', ',']
+        j = 0
+        while len(awards_temp) < 5 and j < len(awards_token):
+            if awards_token[j] not in stopWords_awards:
+                awards_temp.append(awards_token[j])
+                j = j + 1
+            else:
+                j = j + 1
+        awards_keywords.append(awards_temp)
+        keyDict[awards_keys[i]] = awards_temp
+
+
+    
+
+    return keyDict
+
+def get_unique_keys(keyDict):
+    uniquekeys =  keyDict
+    tempKeys = {}
+    for key in keyDict:
+        for item in keyDict[key]:
+            if item in tempKeys:
+                tempKeys[item] += 1
+            else:
+                tempKeys[item] = 1   
+    copyKeys = {}
+    for key in tempKeys:
+        tempKey = key.lower()
+        copyKeys[tempKey] = (tempKeys[key] * -0.4) + 10
+    
+    tempKeys = copyKeys
+
+    
+    print(tempKeys)
+    return tempKeys
+
+
+    
+
+
+    #testVote = voteCounter(nomCand[basicAwards[i]])
+    #testVote = sortVote(testVote)
+    #nomCand[basicAwards[i]] = testVote
+#$nomCand = onlyKnownNames(nomCand)
+
+    
+
+
+#testVote = nipNames(testVote)
+#print(testVote)
+def propNouns(allCan):
+    print("Starting Prop Nouns")
+    nnCand = []
+    nlp = spacy.load("en_core_web_sm")
+    for can in allCan:
+        tweet = nlp(can)
+        for i, tagged_token in enumerate(tweet):
+            tempText = []
+            if tagged_token.pos_ == "PROPN":
+                tempText.append(tagged_token.text)
+                k=i+1
+                while k<len(tweet)-1 and tweet[k].pos_ == "PROPN" and "#" not in tweet.text and "http" not in tweet.text:
+                        tempText.append(tweet[k].text)
+                        k=k+1
+                if len(tempText) >= 2:
+                    tempText = " ".join(tempText)
+                    nnCand.append(tempText)
+                #print("TWEET: " + " ".join(tweet) + " NAME: " +tagged_token[0])
+    with open('propNouns.txt', 'wb') as f:
+        pickle.dump(nnCand, f)
+    print("Finished Prop Nouns")
+    return nnCand
+
+def quickPropNouns(allCan):
+    print("Starting Prop Nouns")
+    nnCand = []
+    nlp = spacy.load("en_core_web_sm")
+    for tweet in nlp.pipe(allCan):
+        for i, tagged_token in enumerate(tweet):
+            tempText = []
+            if tagged_token.pos_ == "PROPN":
+                tempText.append(tagged_token.text)
+                k=i+1
+                while k<len(tweet)-1 and tweet[k].pos_ == "PROPN" and "#" not in tweet[k].text and "http" not in tweet[k].text:
+                        tempText.append(tweet[k].text)
+                        k=k+1
+                if len(tempText) >= 2:
+                    tempText = " ".join(tempText)
+                    nnCand.append(tempText)
+                #print("TWEET: " + " ".join(tweet) + " NAME: " +tagged_token[0])
+    print("Finished Prop Nouns")
+    return nnCand
+
+
+
+def get_nominees_raw(year, awards):
+    
+    word_list = get_tweets(year)
+    awards_keys = list(awards)   
+    awards_keywords = get_awardsKeywords(awards)
+    tweets_stopwords = ['best','better','winner','wins','won','winning','presents','at','for',
+                        'in','to','award','awards','nominees','nominated','congratulates','congratulations',
+                        'congrats','Congrats','was','a','an','i','announced','announce', 'announcing','present',
+                       'by','with','the', 'todayshow','of','latest','movie','film','is','awarded','and',
+                       'actor','globe','or','picture','animated','animation','motion','from']
+    
+    # get tweets with possible nominees
+    nominees_raw = {}
+    for i in range(len(awards_keywords)):
+        nom_values = []
+        # get the matching tweets
+        for j in range(len(word_list)):
+            k = set(awards_keywords[i])
+            b = set(word_list[j])
+            if set(awards_keywords[i]) <= set(word_list[j]):
+                print("IN HERE")
+                wordList_copy = word_list[j].copy()
+                
+                # exclude awards_keywords
+                for ak in awards_keywords[i]:
+                    wordList_copy.remove(ak)
+                # exculde tweets_stopwords
+                wordList = [item for item in wordList_copy if item not in tweets_stopwords]
+                nom_values.append(wordList)
+            else:
+                continue
+
+        nominees_raw[awards_keys[i]] = nom_values
+      
+    return nominees_raw
+
+def matchNames(names, year, awardKeys, award_dict):
+    print("Matching Names...")
+    newDict = {}
+    file_name = 'gg{}.json'.format(year)
+    pand = pandas.read_json(file_name)
+    for row in pand.itertuples():
+        tweet = row[1]
+        tweet = tweet.lower()
+        for name in names:
+            if name in tweet:
+                newAdditions = []
+                for word in awardKeys:
+                    if word in tweet:
+                        newAdditions.append(word)
+                if name in newDict and len(newAdditions) > 0:
+                    for item in newAdditions:
+                        newDict[name].append(item)
+                elif len(newAdditions) > 0:
+                    newDict[name] = newAdditions
+    tempDict = {}
+    print("Finished Matching Names!")
+    for words in newDict:
+        tempDict[words] = sortVote(voteCounter(newDict[words]))
+    return tempDict
+
+def quickMatchNames(names, year, awardKeys):
+    print("Matching Names...")
+    newDict = {}
+    counterDict = {}
+    file_name = 'gg{}.json'.format(year)
+    pand = pandas.read_json(file_name)
+    for name in names:
+        name = name.lower()
+        pand['text'] = pand['text'].str.lower()
+        brandy = pand[pand['text'].str.contains(name)]
+        for word in awardKeys:
+            brandy2 = brandy[brandy['text'].str.contains(word)]
+            counterDict[word] = len(brandy2['text'].tolist())
+        newDict[name] = counterDict
+    tempDict ={}
+    print("Finished Matching Names!")
+    for words in newDict:
+        tempDict[words] = sortVote(newDict[words])
+    return tempDict
+                
+
+def filterNames(names, awardKeys):
+    print("Started Filter Names")
+    alp = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
+    alp = alp.split()
+    filtered = names.copy()
+    for name in names:
+        for award in awardKeys:
+            if award in name or names[name] < 2 or "RT" in name or "golden" in name:
+                if name in filtered:
+                    del(filtered[name])
+    print("Finished Filter Names!")
+    return filtered
+
+def voteCategories(namesAndKeywords, awards):
+    tempDict ={}
+    for keys in namesAndKeywords:
+        tempkeys = namesAndKeywords[keys]
+        if len(tempkeys) >= 5:
+            tempkeys = list(tempkeys)
+            tempkeys = tempkeys[-5:]
+        else:
+            tempkeys = tempkeys
+        tempDict[keys] = tempkeys
+    
+    namesAndKeywords = tempDict
+    posCat = {}
+    for name in namesAndKeywords:
+        for award in awards:
+            for item in awards[award]:
+                item = item.lower()
+                if item in namesAndKeywords[name] and item != "best" and item != "award" and item != "picture" and item != "motion":
+                    if name in posCat:
+                        posCat[name].append(award)
+                    else:
+                        posCat[name] = [award]
+
+    
+    return posCat
+
+        
+def finalizeCategories(nameDict, awardDict):
+    finalAwards = {}
+    for item in awardDict:
+        finalAwards[item] = []
+    for name in nameDict:
+        rev = reversed(list(nameDict[name].keys()))
+        posCat = nameDict[name]
+        for item in rev:
+            if len(finalAwards[item]) >= 4:
+                continue
+            else:
+                finalAwards[item].append(name)
+                break
+    for item in finalAwards:
+        while len(finalAwards[item]) < 4:
+            finalAwards[item].append("NA")
+    return finalAwards
+
+          
+            
+
+def findAllNominees(year):
+    keyDict = get_awardsKeywords(basicAwards)
+    scoreDict = get_unique_keys(keyDict)
+    nomCand = []
+    person_award_criteria = ["actor", "actress", "score", "song", "cecil", "director"]
+    award_dict = {a: [nltk.word_tokenize(a)] for a in basicAwards}
+    temp = {}
+    tempDict = award_dict.copy()
+    for award in tempDict:
+        temp = award.lower()
+        award_dict[temp] = award_dict[award]
+        del(award_dict[award])
+
+    for award in award_dict:
+
+        for item in award_dict[award]:
+            for miniitem in item:
+                miniitem = miniitem.lower()
+                if miniitem in person_award_criteria:
+                    award_dict[award] = "person"
+                    break
+    for award in award_dict:
+        if award_dict[award] != "person":
+            award_dict[award] = "movie"
+    flattenedAwardKeys = []
+    for keyword in keyDict:
+        for item in keyDict[keyword]:
+            flattenedAwardKeys.append(item.lower())
+    flattenedAwardKeys = set(flattenedAwardKeys)
+    nomCand = quickNomGather(award_dict, year)
+    #with open("prereadtweets.txt", 'rb') as f:
+    #    nomCand = pickle.load(f)
+    propN = quickPropNouns(nomCand)
+    propN = voteCounter(propN)
+    propN = filterNames(propN, flattenedAwardKeys)
+    propN = sortVote(propN)
+    propN = matchNames(propN, year, flattenedAwardKeys, award_dict)
+    propN = voteCategories(propN, keyDict)
+    for item in propN:
+        propN[item] = voteCounter(propN[item])
+        propN[item] = sortVote(propN[item])
+    propN = finalizeCategories(propN, keyDict)
+    propN = translate(propN)
+    return propN
+                    
+
+
+
+def main():
+    nominees = findAllNominees(2015)
+    for award in nominees:
+        print(award)
+        print('\n')
+        print(nominees[award])
+        print('\n')
+    return
+
+if __name__ == '__main__':
+    main()
+
+#print(get_nominees_raw(2013, basicAwards))
